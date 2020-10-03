c0d2a8d0b6e4d66bcd2750f239b55ce58ad0916f
wellington <wellingtonslpbi@hotmail.com>
2020-10-03 12:03:13 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -1,7 +1,6 @@
 import { SignUpController } from './signup'
 import { MissingParamError } from '../errors/missing-params-error'
 
-
 describe('SignUpController', () => {
   test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController()
--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,30 +1,22 @@
-import { HttpRequest, HttpResponse}from '../protocols/http'
+import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-params-error'
+import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
+  handle (httpRequest: HttpRequest): HttpResponse {
+    const { name, email } = httpRequest.body
 
-  handle(httpRequest: HttpRequest): HttpResponse {
-    const {name, email} = httpRequest.body
-
-    if(!name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name')
-      }
+    if (!name) {
+      return badRequest(new MissingParamError('name'))
     }
 
-    if(!email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('email')
-      }
+    if (!email) {
+      return badRequest(new MissingParamError('email'))
     }
 
     return {
       statusCode: 400,
       body: {}
     }
-
   }
-
 }
